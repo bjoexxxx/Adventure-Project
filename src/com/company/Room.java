0476fb1c49d12b97d91d5c90e7603e3211c667de
bjoexxxx <bjoe6702@stud.kea.dk>
2022-03-14 14:11:58 +0100

    Added Room Attributes

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -2,13 +2,25 @@ package com.company;
 
 public class Room {
   private String name;
-  public Room(String name){
+  private Room south;
+  private Room north;
+  private Room east;
+  private Room west;
+
+  public Room(String name, Room south, Room north, Room east, Room west){
     this.name = name;
+    this.east = east;
+    this.south = south;
+    this.north = north;
+    this.west = west;
   }
   public void setName(String name){
     this.name = name;
   }
   public String getName(){
     return this.name;
+  }
+  public void goEast(){
+    goeast = get.east.room
   }
 }
