eece77326edf0d1a9df92c90c3cd8af85a2dc7f6
bjoexxxx <bjoe6702@stud.kea.dk>
2022-03-14 13:27:55 +0100

    First Commit

---
+++ src/com/company/Room.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Room {
+  private String name;
+  public Room(String name){
+    this.name = name;
+  }
+  public void setName(String name){
+    this.name = name;
+  }
+  public String getName(){
+    return this.name;
+  }
+}
