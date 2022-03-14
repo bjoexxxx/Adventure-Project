3368f4e040f053856880f2875b5c55fb2497f334
SebastianHeiberg <seba3673@stud.kea.dk>
2022-03-14 15:10:09 +0100

    added build room

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,25 @@ public class Main {
 
     public Scanner keybord = new Scanner(System.in);
 
+    public void buildRooms() {
+        Room room1 = new Room("1","derp");
+        Room room2 = new Room("2","derp");
+        Room room3 = new Room("3","derp");
+        Room room4 = new Room("4","derp");
+        Room room5 = new Room("5","derp");
+        Room room6 = new Room("6","derp");
+        Room room7 = new Room("7","derp");
+        Room room8 = new Room("8","derp");
+        Room room9 = new Room("9","derp");
+
+
+        room1.setEast(room2);
+
+    }
+
     public void MainMenu () {
+
+        buildRooms();
 
         boolean loop = true;
         while (loop) {
@@ -36,19 +54,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Main action = new Main();
-        Room room1 = new Room("1","derp");
-        Room room2 = new Room("2","derp");
-        Room room3 = new Room("3","derp");
-        Room room4 = new Room("4","derp");
-        Room room5 = new Room("5","derp");
-        Room room6 = new Room("6","derp");
-        Room room7 = new Room("7","derp");
-        Room room8 = new Room("8","derp");
-        Room room9 = new Room("9","derp");
 
         new Main().MainMenu();
 
-        room1.setEast(room2);
 
     }
 }
